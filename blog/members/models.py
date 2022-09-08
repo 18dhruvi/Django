73bce89aa14a13d835db4dbe2dbc05e8bd940139
dhruvi <dhruvirajani18@gmail.com>
2022-09-08 14:55:46 +0530

    added templates

--- blog/members/models.py
+++ blog/members/models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.timezone import now
  
@@ -9,7 +9,7 @@ class User(models.Model):
     
     def __str__(self):
         return self.name
-    
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     image = models.ImageField(upload_to="profile_pics", blank=True, null=True)
@@ -17,26 +17,23 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.bio
- 
+
 class BlogPost(models.Model):
     title=models.CharField(max_length=255)
     author= models.ForeignKey(User, on_delete=models.CASCADE)
     content=models.TextField()
     image = models.ImageField(upload_to="profile_pics", blank=True, null=True)
     dateTime=models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.title
-   
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
     comments = models.CharField(max_length=100)
-    # parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)   
     dateTime=models.DateTimeField(default=now)
 
     def __str__(self):
         return self.comments
-    
- 
--- blog/members/urls.py
+++ blog/members/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
-    path('register', views.Register, name='register'),
-    path('blogs',views.blogs, name='blogs'),
+    path('register/', views.Register, name='register'),
+    path('blogs/',views.blogs, name='blogs'),
+    path('login/', views.Login, name='login'),
+    path('logout',views.logout, name='logout'),
+    path('profile/', views.profile,name='profile'),
+    path('blogpost/',views.blogpost, name='blogpost'),
 ]
